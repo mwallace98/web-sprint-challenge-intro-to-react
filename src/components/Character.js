5e1cc6934b17eadf26cd9e1f0d1c6e393bcbf04b
Matt Wallace <mattwallace140@gmail.comgit config --global user.email mattwallace140@gmail.com>
2023-08-13 19:15:15 -0400

    Webpage Complete, new Tests failing, old complete

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,27 @@
 // Write your Character component here
+import React from "react";
+
+
+const characterStyle = {
+    borderRadius: "100px", 
+    padding: "1px",
+    margin: "50px", 
+    backgroundColor:"rgba(242, 242, 242, .3)"
+  };
+
+ 
+
+  export default function Character(props){
+    return (
+        <div style={characterStyle}>
+            <h1>  Name: {props.character.name}</h1>
+            <p>Height: {props.character.height}</p>
+            <p>Birth Year: {props.character.birth_year}</p>
+        </div>
+
+    
+    )     
+}
+
+
+ 
